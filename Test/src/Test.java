2c1538ea651c1faed8c56351a3eda25486a53325
Conor Pointon (POI18161670) <POI18161670@belfastmet.ac.uk>
2018-12-07 15:36:48 +0000

    Added some comments explaining program and functionality of WhaleWatcher class

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,6 +1,13 @@
 
+
+//
+//This is a program that demonstrates OOP programming in Java
+//It also demonstrates many other features of Java such as inheritance, access control, types,
+//data structures etc, in order to output information via the WhaleWatcher class.
+
 public class Test {
 
+	//main method creates whale watcher object that calls multiple methods
 	public static void main(String[] args) {
 		WhaleWatcher whaleWatcher = new WhaleWatcher();
 		
--- Test/src/WhaleWatcher.java
+++ Test/src/WhaleWatcher.java
@@ -9,68 +9,89 @@ public class WhaleWatcher {
 		whales = new ArrayList<>();
 	}
 	
+	//method adds new Whale objects to class property whales list
 	void setWhales() {
-		//add new whales to list
 		whales.add(new Whale("Right", "Atlantic", 2001, 21, 16));
 		whales.add(new Whale("Blue", "Pacific", 2001, 23, 16));
 		whales.add(new Whale("Sperm", "Atlantic", 1900, 20, 40));
 		whales.add(new Whale("Humpback", "Antarctic", 919, 13, 13));
 	}
 	
+	//method loops over whales list and calls each Whale objects toString method
 	void getWhaleInfo() {
 		for(Whale whale : whales) {
 			System.out.println(whale.toString());
 		}
 	}
 	
+	//method loops over whales list to output name of all Atlantic whales
 	void getAtlanticWhales() {
 		for(Whale whale : whales) {
+			
+			//checks if the current indexed whale is Atlantic based
 			if(whale.getMainOcean().equals("Atlantic")) {
 				System.out.println(whale.getName() + " Whale");
 			}
 		}
 	}
 	
+	//method loops over whales list to output name of fastest whales
 	void getFastestWhales() {
+		//new list to store whale speeds
 		ArrayList<Integer> speeds = new ArrayList<>();
 		
+		//add all whale speeds to list
 		for(Whale whale : whales) {
 			speeds.add(whale.getMaxSpeed());
 		}
 		
+		//sort list from slowest to fastest
+		//default sort is Asc
 		Collections.sort(speeds);
 		
 		for(Whale whale : whales) {
+			//check if current indexed whales speed is equal to the highest speed in sorted list
+			//this is done by getting the length of the list and decrementing this values by 1
+			//as lists are zero based
 			if(whale.getMaxSpeed() == speeds.get(speeds.size() - 1)) {
 				System.out.println(whale.getName() + " Whale");
 			}
 		}
 	}
 	
+	//method calculates the average length of all Whale objects in whales list
 	void getAverageWhaleLength() {
 		int average = 0;
 		int whaleCount;
 		
+		//get total number of whales
 		whaleCount = whales.size();
 		
+		//get total length of all whales
 		for(Whale whale : whales) {
 			average += whale.getLength();
 		}
 		
+		//calculates the average and output
 		average /= whaleCount;
 		System.out.println("The average whale length over " + whaleCount + " whale types is: " + average + "m");
 	}
 	
+	//method calculates the heaviest whales
 	void getHeaviestWhales() {
+		//create new list for weights
 		ArrayList<Integer> weights = new ArrayList<>();
 		
+		//add all whale object weights to list
 		for(Whale whale : whales) {
 			weights.add(whale.getWeight());
 		}
 		
+		//sort list from lightest to heaviest
 		Collections.sort(weights);
 		
 		for(Whale whale : whales) {
+			//check if current indexed whale is equal to heaviest value in list
 			if(whale.getWeight() == weights.get(weights.size() - 1)) {
 				System.out.println(whale.getName() + " Whale");
 			}
